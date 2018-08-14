438c8abb8e9ecc93c3a2592265dd06e4dfcaba00
HalF75 <ricardo.rcarvalho@gmail.com>
2018-08-14 13:44:29 +0200

    Update update file

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,15 +1,33 @@
 const { MongoClient, ObjectID } = require("mongodb");
 const uri = "mongodb://localhost:27017/TodoApp";
 
-(async function() {
-  await MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-      if (err) {
-        return console.log("Unable to connect to MongoDB server");
-      }
-      console.log("Connected to MongoDB server");
-      const db = client.db("TodoApp");
-
-      client.close();
+MongoClient.connect(
+  uri,
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
     }
-  );
-})();
+    console.log("Connected to MongoDB server");
+
+    db.collection("Todos")
+      .findOneAndUpdate(
+        {
+          _id: new ObjectID("5b72992e19a539b3ad311eb4")
+        },
+        {
+          $set: {
+            completed: true
+          }
+        },
+        {
+          returnOriginal: false
+        }
+      )
+      .then(result => {
+        console.log(result);
+      });
+
+    //client.close();
+  }
+);
