7b005699c5873983e2ae41936b7722235d43b8b6
HalF75 <ricardo.rcarvalho@gmail.com>
2018-08-14 14:30:52 +0200

    Update Record in Users collection

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,24 +9,38 @@ MongoClient.connect(
       return console.log("Unable to connect to MongoDB server");
     }
     console.log("Connected to MongoDB server");
+    const db = client.db("TodoApp");
 
-    db.collection("Todos")
-      .findOneAndUpdate(
-        {
-          _id: new ObjectID("5b72992e19a539b3ad311eb4")
-        },
-        {
-          $set: {
-            completed: true
-          }
-        },
-        {
-          returnOriginal: false
-        }
-      )
-      .then(result => {
-        console.log(result);
-      });
+    // db.collection("Todos")
+    //   .findOneAndUpdate(
+    //     {
+    //       _id: new ObjectID("5b72992e19a539b3ad311eb4")
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true
+    //       }
+    //     },
+    //     {
+    //       returnOriginal: false
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Tiago'
+    },
+    { 
+        $set: { name: 'Ricardo'},
+        $inc: { age : 1 }
+    },
+    {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    });
 
     //client.close();
   }
