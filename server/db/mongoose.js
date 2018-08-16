695fda32e01da4e70f590f0e56af470f7806b929
HalF75 <ricardo.rcarvalho@gmail.com>
2018-08-16 09:52:06 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/TodoApp";
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, { useNewUrlParser: true });
